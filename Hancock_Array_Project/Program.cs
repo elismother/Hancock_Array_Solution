f6d4f855c9fa1561ecaae9eb64a78a7c6fca13b1
Brittany Hancock <brittany.hancock@student.centralia.edu>
2023-09-28 21:46:48 -0700

    Add project files.

---
+++ Hancock_Array_Project/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Diagnostics;
+
+namespace Hancock_Array_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Random random = new Random();
+
+            int integer;
+
+            char[] charArray = new char[50];
+
+            bool exists = true;
+
+            do
+            {
+                integer = random.Next(65, 122);
+
+                if ((integer > 64 && integer < 91) || (integer > 96 && integer < 123))
+                {
+                    if ((exists = CharExists(integer)) == false)
+                    {
+                        for (int i = 0; i < charArray.Length; i++)
+                        {
+                            if (charArray[i] == '\0')
+                            {
+                                charArray[i] = (char)integer;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+            } while (charArray[49] == '\0');
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Elapsed time: " + (stopwatch.Elapsed.ToString()));
+
+            bool CharExists(int integer)
+            {
+
+                if (!(exists == Array.Exists(charArray, ch => ch == (char)integer)))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
